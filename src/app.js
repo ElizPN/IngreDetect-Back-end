04c127925f092a2edce23afd76a863548c5ec98a
ElizPN <pankevych.liza@gmail.com>
2024-07-03 15:48:01 +0200

    add cors policy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const {
   handleIngridientsRequest,
 } = require("./controllers/ingridientsController");
 
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/data", handleIngridientsRequest);
